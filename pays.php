ce49bac12dae8e2bdc6338073af95638bd18d2ce
alejandro yanez del rio <alex_ydr@hotmail.com>
2024-05-31 15:36:46 -0400

    création du code pour pays.php

--- pays.php
+++ pays.php
@@ -1,55 +1,54 @@
 <?php
 
 /**
- * Package Voyage
+ * Package pays
  * Version 1.0.0
  */
 /*
-Plugin name: Voyage
+Plugin name: pays
 Plugin uri: https://github.com/eddytuto
 Version: 1.0.0
 Description: Permet d'afficher les destinations qui répondent à certains critères
 */
-function aydr_voyage_enqueue()
+function aydr_pays_enqueue()
 {
-    // filemtime // retourne en milliseconde le temps de la dernière modification
-    // plugin_dir_path // retourne le chemin du répertoire du plugin
-    // __FILE__ // le fichier en train de s'exécuter
-    // wp_enqueue_style() // Intègre le link:css dans la page
-    // wp_enqueue_script() // intègre le script dans la page
-    // wp_enqueue_scripts // le hook
-
     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/voyage.js");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/pays.js");
     wp_enqueue_style(
-        'em_plugin_voyage_css',
+        'em_plugin_pays_css',
         plugin_dir_url(__FILE__) . "style.css",
         array(),
         $version_css
     );
 
     wp_enqueue_script(
-        'em_plugin_voyage_js',
-        plugin_dir_url(__FILE__) . "js/voyage.js",
+        'em_plugin_pays_js',
+        plugin_dir_url(__FILE__) . "js/pays.js",
         array(),
         $version_js,
         true
     );
 }
-add_action('wp_enqueue_scripts', 'aydr_voyage_enqueue');
+add_action('wp_enqueue_scripts', 'aydr_pays_enqueue');
+
 /* Création de la liste des destinations en HTML */
 function creation_destinations()
 {
     $contenu = '
-    <button class="boutton_categorie" id="cat_2">Aventure</button>
-    <button class="boutton_categorie" id="cat_3">Culturel</button>
-    <button class="boutton_categorie" id="cat_4">Repos</button>
-    <button class="boutton_categorie" id="cat_5">Zen</button>
-    <button class="boutton_categorie" id="cat_6">Sport</button>
-    <button class="boutton_categorie" id="cat_7">Économique</button>
-    <button class="boutton_categorie" id="cat_8">Croisière</button>
-    <button class="boutton_categorie" id="cat_9">Pleine nature</button>
-    
+    <button class="boutton_categorie" id="cat_2">France</button>
+    <button class="boutton_categorie" id="cat_3">États-Unis</button>
+    <button class="boutton_categorie" id="cat_4">Canada</button>
+    <button class="boutton_categorie" id="cat_5">Argentine</button>
+    <button class="boutton_categorie" id="cat_6">Chili</button>
+    <button class="boutton_categorie" id="cat_7">Belgique</button>
+    <button class="boutton_categorie" id="cat_8">Maroc</button>
+    <button class="boutton_categorie" id="cat_9">Mexique</button>
+    <button class="boutton_categorie" id="cat_10">Japon</button>
+    <button class="boutton_categorie" id="cat_11">Italie</button>
+    <button class="boutton_categorie" id="cat_12">Islande</button>
+    <button class="boutton_categorie" id="cat_13">Chine</button>
+    <button class="boutton_categorie" id="cat_14">Grèce</button>
+    <button class="boutton_categorie" id="cat_15">Suisse</button>
     
     <div class="contenu__restapi">
     </div>';
